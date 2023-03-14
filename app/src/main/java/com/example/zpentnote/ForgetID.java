1d8f12412e16d3ab191ed148cc03d983ecfd1ba1
ZMILE <61521000216@mail.rmutk.ac.th>
2023-03-14 22:44:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/zpentnote/ForgetID.java
@@ -0,0 +1,14 @@
+package com.example.zpentnote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgetID extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_id);
+    }
+}

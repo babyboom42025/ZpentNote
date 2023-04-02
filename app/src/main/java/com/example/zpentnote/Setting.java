dd1f8dfecfa0a8efb0e0029e19302e0961321856
ZMILE <61521000216@mail.rmutk.ac.th>
2023-04-02 21:59:35 +0700

    Update Notification switch,AboutUs link to URL.

--- app/src/main/java/com/example/zpentnote/Setting.java
+++ app/src/main/java/com/example/zpentnote/Setting.java
@@ -1,18 +1,25 @@
 package com.example.zpentnote;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class Setting extends AppCompatActivity {
 
-    RelativeLayout editgoal;
+    RelativeLayout editgoal,aboutUs;
     ImageView profileBtn;
+
+    SwitchCompat notification;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +28,8 @@ public class Setting extends AppCompatActivity {
 
         editgoal=findViewById(R.id.editgoal);
         profileBtn=findViewById(R.id.profileBtn);
+        notification=findViewById(R.id.Notification);
+        aboutUs=findViewById(R.id.aboutUs);
         editgoal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,5 +43,29 @@ public class Setting extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(),Profile.class));
             }
         });
+
+        notification.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+
+                if (isChecked){
+                    Toast.makeText(getApplicationContext(),"Notification is ON",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(getApplicationContext(),"Notification is OFF",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        aboutUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.google.co.th/";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
     }
 }
